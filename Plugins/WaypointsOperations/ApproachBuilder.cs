b4aee0cdbf79fefb07ef78b0b4dacfa80c46a1fd
omysnyk <a.m.f.aidan@gmail.com>
2026-04-07 02:23:49 +0300

    Add additional validations to approach builder

--- Plugins/WaypointsOperations/ApproachBuilder.cs
+++ Plugins/WaypointsOperations/ApproachBuilder.cs
@@ -19,58 +19,77 @@ namespace WaypointsOperations
             this.WaypointsPlugin = waypointsPlugin;
         }
 
-        static double ToRadians(double degrees) {
+        static double ToRadians(double degrees)
+        {
             return degrees * Math.PI / 180.0;
         }
 
-        public void AddApproachPoints() {
+        public void AddApproachPoints()
+        {
             var approachAlt = 60;
             if (InputBox.Show("Approach Parameters", "Select approach altitude above target", ref approachAlt) !=
-                DialogResult.OK) {
+                DialogResult.OK)
+            {
+                return;
+            }
+
+            if (!ApproachDescentAngles.TryGetValue(approachAlt, out var approachDescentAngle))
+            {
+                CustomMessageBox.Show("Unsupported approach altitude.",
+                    "Approach calculate error", CustomMessageBox.MessageBoxButtons.OK,
+                    CustomMessageBox.MessageBoxIcon.Error);
                 return;
             }
 
             var flightPlanner = MainV2.instance.FlightPlanner;
             var commands = flightPlanner.Commands;
-            
+
             var routePoints = flightPlanner.pointlist;
-            var routeLastPoints = flightPlanner.pointlist.Skip(routePoints.Count - 2);
+            var routeLastPoints = (routePoints.AsEnumerable() ?? Array.Empty<PointLatLngAlt>())
+                .Reverse()
+                .Where(point => point != null && point.Tag != "H")
+                .Take(2)
+                .Reverse();
+
             var finalApproachVector = routeLastPoints as PointLatLngAlt[] ?? routeLastPoints.ToArray();
-            if (finalApproachVector.Length < 2 || !finalApproachVector.All(point => point != null && point.Tag != "H")) {
+            if (finalApproachVector.Length < 2)
+            {
                 CustomMessageBox.Show("At least two waypoints needed at the end of a mission!",
-                    "Approach calculate error", CustomMessageBox.MessageBoxButtons.OK, CustomMessageBox.MessageBoxIcon.Error);
+                    "Approach calculate error", CustomMessageBox.MessageBoxButtons.OK,
+                    CustomMessageBox.MessageBoxIcon.Error);
                 return;
             }
 
             var endIndex = routePoints.Count(alt => alt?.Tag != "H") - 1; // exclude home if present
-            var lastPoint = routePoints[routePoints.Count - 1];
-            var nextToLastPoint = routePoints[routePoints.Count - 2];
+            var lastPoint = finalApproachVector[1];
+            var nextToLastPoint = finalApproachVector[0];
 
-            /*var allowedCommands = ImmutableHashSet.Create(
-                (ushort)MAVLink.MAV_CMD.LAND,
-                (ushort)MAVLink.MAV_CMD.WAYPOINT);
-
-            var isLastCommandsValid = new[] { ExtractCmd(lastCommand), ExtractCmd(nextToLastCommand) }.All(val => allowedCommands.Contains(val));/*/
-            if (lastPoint == null || nextToLastPoint == null) {
+            if (lastPoint == null || nextToLastPoint == null)
+            {
                 CustomMessageBox.Show("Require WAYPOINT/LAND as two last mission command items!",
                     "Error", CustomMessageBox.MessageBoxButtons.OK,
                     CustomMessageBox.MessageBoxIcon.Error);
                 return;
             }
 
-            // var endCoords = ExtractCoords(lastPoint);
-            // var startCoords = ExtractCoords(nextToLastPoint);
-
             var lastCommandRow = commands.Rows[commands.Rows.Count - 1];
             lastCommandRow.Cells[0].Value = nameof(MAVLink.MAV_CMD.LAND);
             lastCommandRow.Cells[7].Value = lastPoint.Alt;
 
-            var line = Geod.InverseLine(nextToLastPoint.Lat, nextToLastPoint.Lng, lastPoint.Lat,  lastPoint.Lng);
+            var line = Geod.InverseLine(nextToLastPoint.Lat, nextToLastPoint.Lng, lastPoint.Lat, lastPoint.Lng);
+            if (line.Distance < 2000)
+            {
+                CustomMessageBox.Show("Unable to build approach on route leg less then 2000 meters.",
+                    "Approach calculate error", CustomMessageBox.MessageBoxButtons.OK,
+                    CustomMessageBox.MessageBoxIcon.Warning);
+                return;
+            }
+
             var verifyCheckerState = flightPlanner.CHK_verifyheight.Checked;
             var distFromEnd = Math.Max(0, line.Distance - 2000);
             var newPoint = line.Position(distFromEnd);
             var aimAlt = lastPoint.Alt + approachAlt;
-            
+
             flightPlanner.CHK_verifyheight.CheckState = CheckState.Unchecked;
             flightPlanner.CHK_verifyheight.Checked = false;
             WaypointsPlugin.Host.InsertWP(endIndex, MAVLink.MAV_CMD.WAYPOINT, 0, 0, 0, 0,
@@ -89,7 +108,6 @@ namespace WaypointsOperations
                 newPoint.Longitude, newPoint.Latitude,
                 aimAlt);
 
-            var approachDescentAngle = ApproachDescentAngles[approachAlt];
             var aimPointDistance = approachAlt * 1.0 / Math.Tan(ToRadians(approachDescentAngle));
             distFromEnd = Math.Max(0, line.Distance - aimPointDistance);
             newPoint = line.Position(distFromEnd);
@@ -97,35 +115,17 @@ namespace WaypointsOperations
                 newPoint.Longitude, newPoint.Latitude,
                 aimAlt);
 
-
-            // var targetApproachAltAsl = (int)Math.Round(srtm.getAltitude(newPoint.Latitude, newPoint.Longitude).alt);
             flightPlanner.CHK_verifyheight.Checked = verifyCheckerState;
-
-            // var intervalsCount = (int)Math.Ceiling(line.Distance / 10.0);
-            // for (int i = 0; i < intervalsCount; i++) {
-            //     var probeCoord = line.Position(Math.Min(line.Distance, 10 * intervalsCount));
-            // }
         }
 
         private static readonly Geodesic Geod = Geodesic.WGS84;
-        private static readonly Dictionary<int, int> ApproachDescentAngles = new Dictionary<int, int> {
+
+        private static readonly Dictionary<int, int> ApproachDescentAngles = new Dictionary<int, int>
+        {
             { 40, 18 },
             { 60, 23 },
             { 80, 26 },
             { 100, 30 }
         };
-
-        private static (double lat, double lon, double alt) ExtractCoords(DataGridViewRow lastCommand) {
-            var lat = double.Parse(lastCommand.Cells[5].Value.ToString());
-            var lon = double.Parse(lastCommand.Cells[6].Value.ToString());
-            var alt = double.Parse(lastCommand.Cells[7].Value.ToString()) / CurrentState.multiplieralt;
-
-            return (lat: lat, lon: lon, alt: alt);
-        }
-
-
-        private ushort ExtractCmd(DataGridViewRow row) {
-            return MainV2.instance.FlightPlanner.getCmdID(row.Cells[0].Value.ToString());
-        }
     }
 }
